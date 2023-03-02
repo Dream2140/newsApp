6bfcbda9ed661922c5a838b714653fe56c0ef6b3
Dream2140 <glebantonenko2140@gmail.com>
2023-03-02 15:49:18 +0200

    Cloned client content to client branch

--- api/app/helpers/variables.js
+++
@@ -1,7 +0,0 @@
-module.exports = class Variables {
-    static cybersportNewsLineLink = 'https://www.cybersport.ru/api/materials?pageoffset=0&pagelimit=200&sort=-publishedAt';
-    static cybersportOneNewsLink = 'https://www.cybersport.ru/api/materials/';
-    static cybersportImageLink = 'https://virtus-img.cdnvideo.ru/images/material-card/plain/';
-    static defaultNewsLimit = 8;
-    static guardianNewsLineLink = 'https://content.guardianapis.com/search';
-}
